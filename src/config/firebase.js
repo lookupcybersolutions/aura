160e3d63c1b1eb57696912f5844d41a9044f9acb
Swarup Solanki <64232307+swarup-coder@users.noreply.github.com>
2020-09-25 23:50:23 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,16 @@ import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/messaging';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: "AIzaSyCmEzsTpfTvWI5mH3Ys3ziYl0U_GCqzKwo",
+  authDomain: "lookup-cyber-solutions-b378d.firebaseapp.com",
+  databaseURL: "https://lookup-cyber-solutions-b378d.firebaseio.com",
+  projectId: "lookup-cyber-solutions-b378d",
+  storageBucket: "lookup-cyber-solutions-b378d.appspot.com",
+  messagingSenderId: "931993750569",
+  appId: "1:931993750569:web:b7512a5475172262876038",
+  measurementId: "G-Q1R52VMY4N"
 };
 
 firebase.initializeApp(firebaseConfig);
